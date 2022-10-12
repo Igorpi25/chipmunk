a74178d7f2573b0983bb55d31925cc43f43c96a9
igor_ivanov <igorpi25@gmail.com>
2022-10-13 00:00:17 +0900

    remove temporary mock repositories

--- lib/repositories/market_repository.dart
+++
@@ -1,6 +0,0 @@
-const _markets = ['Forex', 'HKS', 'Yandex', 'Nasa'];
-const _delay = Duration(seconds: 1);
-
-class MarketRepository {
-  Future<List<String>> loadMarkets() => Future.delayed(_delay, () => _markets);
-}
--- lib/repositories/price_repository.dart
+++
@@ -1,7 +0,0 @@
-const _delay = Duration(seconds: 1);
-
-class PriceRepository {
-  Stream<String> tick(String asset) {
-    return Stream.periodic(_delay, (_) => '$asset : $_');
-  }
-}
